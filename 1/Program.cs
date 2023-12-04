6f8d94e867179a0fac61103d1a2560d8bbb6efa9
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-12-04 19:33:56 +0100

    Finished first assignment

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace _1;
+﻿using System.Text.RegularExpressions;
+
+namespace _1;
 
 class Program
 {
-    static void Main(string[] args)
+    private static Regex digitsRegex = new(@"(?(?=^(?:[^\d]*)(?:\d)(?:.*)(?:\d)[^\d]*$)^(?:[^\d]*)(?<firstdigit>\d)(?:.*)(?<seconddigit>\d)[^\d]*$|^(?:[^\d]*)(?<firstdigit>\d))", RegexOptions.Compiled);
+
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int result = (from line in File.ReadAllLines("input.txt")
+            select digitsRegex.Match(line)
+            into match
+            let firstDigit = match.Groups["firstdigit"].Value
+            let secondDigit = match.Groups["seconddigit"].Value == string.Empty
+                ? firstDigit
+                : match.Groups["seconddigit"].Value
+            select Convert.ToInt32($"{firstDigit}{secondDigit}")).ToList().Sum();
+
+        Console.Write($"Result: {result}");
     }
 }
