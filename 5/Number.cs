eda2f85fb2b9f7fc56e418e3f9f4dca811be3375
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2023-12-11 20:28:27 +0100

    Add Number class

---
+++ 5/Number.cs
@@ -0,0 +1,15 @@
+﻿namespace _5;
+
+public class Number
+{
+    public int Value { get; init; }
+    private int LineNumber { get; init; }
+    private int Index { get; init; }
+
+    public Number(int value, int lineNumber, int index)
+    {
+        Value = value;
+        LineNumber = lineNumber;
+        Index = index;
+    }
+}
--- 5/Solver.cs
+++ 5/Solver.cs
@@ -1,12 +1,36 @@
-﻿namespace _5;
+﻿using System.Text.RegularExpressions;
 
-public class Solver
+namespace _5;
+
+public partial class Solver
 {
+    private char[][] matrix;
+    private IList<Number> Numbers = new List<Number>();
+
     public void Solve()
     {
+        int lineNumber = 0;
         foreach (string line in File.ReadLines("input.txt"))
         {
-
+            ExtractNumbers(line, lineNumber);
+            lineNumber++;
         }
     }
+
+    private void ExtractNumbers(string line, int lineNumber)
+    {
+        foreach (Match? match in MatchNumbers().Matches(line))
+        {
+            if (match != null)
+            {
+                int number = Convert.ToInt32(match.Groups["Number"].Value);
+                int index = line.IndexOf(match.Groups["Number"].Value, StringComparison.Ordinal);
+                Numbers.Add(new Number(number, lineNumber, index));
+            }
+        }
+
+    }
+
+    [GeneratedRegex(@"[^0-9]*(?<Number>\d{1,})")]
+    private static partial Regex MatchNumbers();
 }
